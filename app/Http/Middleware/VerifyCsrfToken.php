259bbf4b23160b51e96ddbf3795858711e5a3f56
shahram peyvandi <yasfuny@gmail.com>
2020-04-20 22:13:17 -0700

    یبس

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'admin/panel/upload-image','admin/panel/upload-image2'
+        'panel/upload-image','panel/upload-image2'
     ];
 }
